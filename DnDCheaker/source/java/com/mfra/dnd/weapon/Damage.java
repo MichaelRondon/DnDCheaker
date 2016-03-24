fcb3d5e464c9b7012a58321eda2f4564ab6d2c38
MichaelRondon <mfrondona@unal.edu.co>
2016-03-24 11:13:01 -0500

    Damage fix.

--- DnDCheaker/source/java/com/mfra/dnd/weapon/Damage.java
+++ DnDCheaker/source/java/com/mfra/dnd/weapon/Damage.java
@@ -1,48 +1,47 @@
 package com.mfra.dnd.weapon;
 
 import java.util.HashMap;
-import com.mfra.dice.Dice;
 import com.mfra.dice.IDice;
 import com.mfra.dnd.checker.ACheckeable;
 import com.mfra.dnd.checker.Ability.AbilityName;
 import com.mfra.dnd.checker.AbilityWorker;
+import com.mfra.dnd.util.IBasicData;
 
 /**
  * @author Michael Felipe Rondón Acosta
  */
 public class Damage extends AbilityWorker {
 
-    private IDice    dice;
+	private IDice dice;
 
-    /**
-     * @param checkProperties
-     * @param abilityName
-     * @param descProperties
-     */
-    public Damage(HashMap<Enum<?>, ACheckeable> checkProperties,
-            AbilityName abilityName, HashMap<String, Object> descProperties) {
-        super(checkProperties, abilityName);
+	/**
+	 * @param checkProperties
+	 * @param abilityName
+	 * @param descProperties
+	 */
+	public Damage(HashMap<Enum<?>, ACheckeable> checkProperties, AbilityName abilityName, IBasicData iBasicData) {
+		super(iBasicData, abilityName);
 
-    }
+	}
 
-    /**
-     */
-    public void makeDamage() {
-        stringBuilder = new StringBuilder();
-        stringBuilder.append("To makes damage ");
-        int resp = 0;
-        if (this.dice != null) {
-            IDice rollDice = this.dice.rollDice();
-            stringBuilder.append(rollDice.getStringFromBuilder());
-            resp += rollDice.getLastValue();
-        }
+	/**
+	 */
+	public void makeDamage() {
+		stringBuilder = new StringBuilder();
+		stringBuilder.append("To makes damage ");
+		int resp = 0;
+		if (this.dice != null) {
+			IDice rollDice = this.dice.rollDice();
+			stringBuilder.append(rollDice.getStringFromBuilder());
+			resp += rollDice.getLastValue();
+		}
 
-    }
+	}
 
-    /**
-     * @param diceModifier
-     */
-    public void setDiceModifier(IDice diceModifier) {
-        this.dice = diceModifier;
-    }
+	/**
+	 * @param diceModifier
+	 */
+	public void setDiceModifier(IDice diceModifier) {
+		this.dice = diceModifier;
+	}
 }
